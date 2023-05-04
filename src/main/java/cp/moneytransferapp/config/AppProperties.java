7a5c8a442c036071b4bc24436bd2ae3f4d3f0751
ilk07 <ilk07@inbox.ru>
2023-05-04 23:31:29 +0300

    configuration properties

--- src/main/java/cp/moneytransferapp/config/AppProperties.java
+++ src/main/java/cp/moneytransferapp/config/AppProperties.java
@@ -2,10 +2,28 @@ package cp.moneytransferapp.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
-@Getter
-@Setter
+@Configuration
+@ConfigurationProperties(prefix = "transfer")
 public class AppProperties {
     private String logFilename;
     private double taxFee;
+
+    public String getLogFilename() {
+        return logFilename;
+    }
+
+    public void setLogFilename(String logFilename) {
+        this.logFilename = logFilename;
+    }
+
+    public double getTaxFee() {
+        return taxFee;
+    }
+
+    public void setTaxFee(double taxFee) {
+        this.taxFee = taxFee;
+    }
 }
--- src/main/java/cp/moneytransferapp/logger/impl/TransferLogger.java
+++ src/main/java/cp/moneytransferapp/logger/impl/TransferLogger.java
@@ -1,31 +1,30 @@
 package cp.moneytransferapp.logger.impl;
 
+import cp.moneytransferapp.config.AppProperties;
 import cp.moneytransferapp.logger.TransferJournal;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Properties;
 
 @Getter
 @Setter
 @Component
 public class TransferLogger implements TransferJournal {
 
-//    @Autowired
-//    private ApplicationContext appContext;
-
+    @Autowired
+    AppProperties appProperties;
     private static TransferLogger INSTANCE = null;
-    private String filename;
 
     DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
-    private TransferLogger() {
-        this.filename = getFileNameFromPropertyFile();
-    }
+    private TransferLogger() {}
 
     public static TransferLogger getInstance() {
         if (INSTANCE == null) {
@@ -43,7 +42,7 @@ public class TransferLogger implements TransferJournal {
     }
 
     private void writeToFile(String msg) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename, true))) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(appProperties.getLogFilename(), true))) {
             bw.write(createLogMessage(msg));
             bw.write('\n');
             bw.flush();
@@ -51,19 +50,6 @@ public class TransferLogger implements TransferJournal {
             ex.printStackTrace();
         }
     }
-
-    private String getFileNameFromPropertyFile() {
-        try (InputStream input = new FileInputStream("./src/main/resources/application.properties")) {
-            Properties prop = new Properties();
-            prop.load(input);
-            return prop.getProperty("transfer.logFilename");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        return null;
-
-    }
-
 
     @Override
     public boolean logToJournal(String msg) {
